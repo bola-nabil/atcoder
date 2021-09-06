70bd91780240880ff039646e69a396fc5218e288
Bola Nabil <68195032+Bola187@users.noreply.github.com>
2021-09-06 20:47:35 +0200

    atcoder solutions in cpp

---
+++ AtCoder  Contest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+
+    cout << "A" << s2.at(0) << "C" << endl;
+    return 0;
+}
