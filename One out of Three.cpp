3123124665d1b6ae7eae52f4585dd058ed631b23
Bola Nabil <68195032+Bola187@users.noreply.github.com>
2021-09-07 21:12:12 +0200

    atcoder solutions in cpp

---
+++ One out of Three.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a == b) cout << c << endl;
+    else if (a == c) cout << b << endl;
+    else if (b == c) cout << a << endl;
+    return 0;
+}
