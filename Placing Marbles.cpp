81018d7a4ace31e45b500716805adfdc3bf271c8
Bola Nabil <68195032+Bola187@users.noreply.github.com>
2021-09-07 21:57:19 +0200

    atcoder solutions in cpp

---
+++ Placing Marbles.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,sum=0,m;
+    cin >> n;
+
+    while (n > 0)
+    {
+        m = n % 10;
+        sum += m;
+        n /= 10;
+    }
+    cout << sum << endl;
+    return 0;
+}
