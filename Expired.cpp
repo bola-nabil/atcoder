cce74695207494f5d66443456bdd05bc8b451976
Bola Nabil <68195032+Bola187@users.noreply.github.com>
2021-09-07 11:12:06 +0200

    atcoder solutions in cpp

---
+++ Expired.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, a, b;
+    cin >> x >> a >> b;
+
+    if ((b - a) <= 0)
+        cout << "delicious" << endl;
+    else if ((b - a) <= x)
+        cout << "safe" << endl;
+    else
+        cout << "dangerous" << endl;
+    return 0;
+}
