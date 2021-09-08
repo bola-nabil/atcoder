28e2c566bd48fd8dbc375fc4bbf1c17a64f21fd6
Bola Nabil <68195032+Bola187@users.noreply.github.com>
2021-09-08 13:16:37 +0200

    atcoder solutions in cpp

---
+++ Diagonal String.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+
+    cout << s1[0] << s2[1] << s3[2] << endl;
+    return 0;
+}
