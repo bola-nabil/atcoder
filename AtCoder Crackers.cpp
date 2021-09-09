465f10fdc2071c5d1b31f54ec09f99384f2e9a56
Bola Nabil <68195032+Bola187@users.noreply.github.com>
2021-09-09 15:04:49 +0200

    atcoder solutions in cpp

---
+++ AtCoder Crackers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    if (n % k)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+    return 0;
+}
